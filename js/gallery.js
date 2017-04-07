2d19fe45caaeebe85bf4ff74aeebcd726b499850
katy-400 <katy.nyby@gmail.com>
2017-04-06 22:25:34 -0400

    fixed getElementByID to ClassName

--- js/gallery.js
+++ js/gallery.js
@@ -44,9 +44,9 @@ function swapPhoto() {
 	
 	var curImg = mImages[mCurrentIndex];
 	document.getElementById("photo").src = curImg.imgPath;
-	document.getElementById("location")[0].innerHTML = "Location: " + curImg.imgLocation;
-	document.getElementById("description")[0].innerHTML = "Description: " + curImg.description;
-	document.getElementById("date")[0].innerHTML = "Date" + curImg.date;
+	document.getElementByClassName("location")[0].innerHTML = "Location: " + curImg.imgLocation;
+	document.getElementByClassName("description")[0].innerHTML = "Description: " + curImg.description;
+	document.getElementByClassName("date")[0].innerHTML = "Date" + curImg.date;
 	
 	console.log('swap photo');
 }
